44eb553238d0c3b7c1f0ed35b5b8ae1234000692
maypok86 <alex.mayshev.86@gmail.com>
2023-02-12 14:50:19 +0300

    feat: add prometheus

--- monitoring/deploy.sh
+++ monitoring/deploy.sh
@@ -7,6 +7,7 @@ readonly port=$2
 
 ssh -o StrictHostKeyChecking=no deploy@"$host" -p "$port" 'mkdir -p configs'
 scp -r -o StrictHostKeyChecking=no -P "$port" alertmanager deploy@"$host":configs/alertmanager
+scp -r -o StrictHostKeyChecking=no -P "$port" prometheus deploy@"$host":configs/prometheus
 ssh -o StrictHostKeyChecking=no deploy@"$host" -p "$port" 'docker network create --driver=overlay traefik-public || true'
 ssh -o StrictHostKeyChecking=no deploy@"$host" -p "$port" 'docker network create --driver=overlay monitoring-public || true'
 ssh -o StrictHostKeyChecking=no deploy@"$host" -p "$port" 'rm -rf monitoring && mkdir monitoring'
