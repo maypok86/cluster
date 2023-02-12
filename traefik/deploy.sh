76047dd7c10d1729a666cc733e49457604b1c58d
maypok86 <alex.mayshev.86@gmail.com>
2023-02-12 14:06:28 +0300

    feat: enable logging and monitoring in traefik

--- traefik/deploy.sh
+++ traefik/deploy.sh
@@ -6,6 +6,7 @@ readonly host=$1
 readonly port=$2
 
 ssh -o StrictHostKeyChecking=no deploy@"$host" -p "$port" 'docker network create --driver=overlay traefik-public || true'
+ssh -o StrictHostKeyChecking=no deploy@"$host" -p "$port" 'docker network create --driver=overlay monitoring-public || true'
 ssh -o StrictHostKeyChecking=no deploy@"$host" -p "$port" 'rm -rf traefik && mkdir traefik'
 scp -o StrictHostKeyChecking=no -P "$port" docker-compose-production.yml deploy@"$host":traefik/docker-compose.yml
 ssh -o StrictHostKeyChecking=no deploy@"$host" -p "$port" 'cd traefik && docker stack deploy -c docker-compose.yml traefik'
